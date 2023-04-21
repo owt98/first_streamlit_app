503986687c450fdcc4883cf0d4f3d79ad41cf560
owt98 <129615881+owt98@users.noreply.github.com>
2023-04-21 13:25:53 +0100

    Update streamlit_app.py
    new smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit
 
 streamlit.header(' 🥣 🥗Breakfast Menu')
 streamlit.text(' 🥣 🥗 🐔 🥑🍞Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🥣 🥗 🐔 🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥣 🥗 🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🥑🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
