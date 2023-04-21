9d394ee48ebd1394769dac699abea8a7bbfb5f26
owt98 <129615881+owt98@users.noreply.github.com>
2023-04-21 13:24:42 +0100

    Update streamlit_app.py
    adding emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(' 🥣 🥗Breakfast Menu')
+streamlit.text(' 🥣 🥗 🐔 🥑🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🥣 🥗 🐔 🥑🍞Hard-Boiled Free-Range Egg')
