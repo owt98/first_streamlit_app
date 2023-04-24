e3881144ebad4dffd13b4d43d98e6bd2c7156522
owt98 <129615881+owt98@users.noreply.github.com>
2023-04-24 10:31:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,10 @@ import snowflake.connector
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('' from streamline')")
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
