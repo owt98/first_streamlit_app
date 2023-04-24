70166d9438ecfe9b3478a3373431b1d8d2fce217
owt98 <129615881+owt98@users.noreply.github.com>
2023-04-24 10:49:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ except URLError as e:
 
 # take the json version of the response anfd normalize it
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 #don't run anything past here we want to troubleshoot
 streamlit.stop()
